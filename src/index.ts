b0698eca93df026c9b2025d08454993c0c6c8bda
文才 <wencai@66law.cn>
2025-09-01 09:41:38 +0800

    chore: 支持邮件转发

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,13 @@
 import * as PostalMime from 'postal-mime'
-import * as mimeDb from 'mime-db'
 
-import * as unzipit from 'unzipit'
-import * as pako from 'pako'
-
-import { XMLParser } from 'fast-xml-parser'
 
 import {
   Env,
-  Attachment,
-  DmarcRecordRow,
-  AlignmentType,
-  DispositionType,
-  DMARCResultType,
-  PolicyOverrideType,
 } from './types'
 
-export default {
-  async email(message: EmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
-    await handleEmail(message, env, ctx)
-  },
-}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function handleEmail(message: EmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
+async function handleEmail(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
   const parser = new PostalMime.default()
 
   // parse email content
@@ -31,152 +15,47 @@ async function handleEmail(message: EmailMessage, env: Env, ctx: ExecutionContex
 
   const email = await parser.parse(await rawEmail.arrayBuffer())
 
-  // get attachment
-  if (email.attachments === null || email.attachments.length === 0) {
-    throw new Error('no attachments')
-  }
-  const attachment = email.attachments[0]
+  // // get attachment
+  // if (email.attachments === null || email.attachments.length === 0) {
+  //     throw new Error('no attachments')
+  // }
+  // const attachment = email.attachments[0]
 
-  // save on R2
-  if (env.R2_BUCKET) {
-    const date = new Date()
-    await env.R2_BUCKET.put(
-      `${date.getUTCFullYear()}/${date.getUTCMonth() + 1}/${attachment.filename}`,
-      attachment.content
-    )
+  const body = {
+    "msgtype": "markdown",
+    "markdown": {
+      "content": `<font color="warning">【你有新邮件，请及时处理】</font>\n主题:<font color="warning">${message.headers.get('subject')}</font>\n发件人:<font color="comment">${message.from}</font>\n收件人:<font color="comment">${message.to}</font>\n> <font color="comment">${email.text??"无内容"}</font>`
+    }
   }
 
-  // get xml
-  const reportJSON = await getDMARCReportXML(attachment)
-
-  // get report
-  const report = getReportRows(reportJSON)
-
-  // send to analytics engine
-  await sendToAnalyticsEngine(env, report)
+  if (!await sendToWebhook(body)) {
+    message.forward("laoshancun@foxmail.com")
+  }
 }
 
-async function getDMARCReportXML(attachment: Attachment) {
-  let xml
-  const xmlParser = new XMLParser()
-  const extension = mimeDb[attachment.mimeType]?.extensions?.[0] || ''
-
-  switch (extension) {
-    case 'gz':
-      xml = pako.inflate(new TextEncoder().encode(attachment.content as string), { to: 'string' })
-      break
-
-    case 'zip':
-      xml = await getXMLFromZip(attachment.content)
-      break
-
-    case 'xml':
-      xml = await new Response(attachment.content).text()
-      break
-
-    default:
-      throw new Error(`unknown extension: ${extension}`)
-  }
-
-  return await xmlParser.parse(xml)
+async function sendToWebhook(body: any) {
+  const req = new Request("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=57720378-4f5b-4fca-a69d-7f340a960ff5", {
+    method: "POST",
+    body: JSON.stringify(body)
+  })
+  req.headers.append("Content-Type", "application/json")
+  const res = await fetch(req);
+  return res.ok
 }
 
-async function getXMLFromZip(content: string | ArrayBuffer | Blob | unzipit.TypedArray | unzipit.Reader) {
-  const { entries } = await unzipit.unzipRaw(content)
-  if (entries.length === 0) {
-    return new Error('no entries in zip')
-  }
-
-  return await entries[0].text()
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function getReportRows(report: any): DmarcRecordRow[] {
-  const reportMetadata = report.feedback.report_metadata
-  const policyPublished = report.feedback.policy_published
-  const records = Array.isArray(report.feedback.record) ? report.feedback.record : [report.feedback.record]
-
-  if (!report.feedback || !reportMetadata || !policyPublished || !records) {
-    throw new Error('invalid xml')
-  }
-
-  const listEvents: DmarcRecordRow[] = []
-
-  for (let index = 0; index < records.length; index++) {
-    const record = records[index]
-
-    const reportRow: DmarcRecordRow = {
-      reportMetadataReportId: reportMetadata.report_id.toString().replace('-', '_'),
-      reportMetadataOrgName: reportMetadata.org_name || '',
-      reportMetadataDateRangeBegin: parseInt(reportMetadata.date_range.begin) || 0,
-      reportMetadataDateRangeEnd: parseInt(reportMetadata.date_range.end) || 0,
-      reportMetadataError: JSON.stringify(reportMetadata.error) || '',
-
-      policyPublishedDomain: policyPublished.domain || '',
-      policyPublishedADKIM: AlignmentType[policyPublished.adkim as keyof typeof AlignmentType],
-      policyPublishedASPF: AlignmentType[policyPublished.aspf as keyof typeof AlignmentType],
-      policyPublishedP: DispositionType[policyPublished.p as keyof typeof DispositionType],
-      policyPublishedSP: DispositionType[policyPublished.sp as keyof typeof DispositionType],
-      policyPublishedPct: parseInt(policyPublished.pct) || 0,
-
-      recordRowSourceIP: record.row.source_ip || '',
-
-      recordRowCount: parseInt(record.row.count) || 0,
-      recordRowPolicyEvaluatedDKIM: DMARCResultType[record.row.policy_evaluated.dkim as keyof typeof DMARCResultType],
-      recordRowPolicyEvaluatedSPF: DMARCResultType[record.row.policy_evaluated.spf as keyof typeof DMARCResultType],
-      recordRowPolicyEvaluatedDisposition:
-        DispositionType[record.row.policy_evaluated.disposition as keyof typeof DispositionType],
-
-      recordRowPolicyEvaluatedReasonType:
-        PolicyOverrideType[record.row.policy_evaluated?.reason?.type as keyof typeof PolicyOverrideType],
-      recordIdentifiersEnvelopeTo: record.identifiers.envelope_to || '',
-      recordIdentifiersHeaderFrom: record.identifiers.header_from || '',
+export default {
+  async email(message: ForwardableEmailMessage, env: Env, ctx: ExecutionContext): Promise<void> {
+    try {
+      await handleEmail(message, env, ctx)
+    } catch (error) {
+      const body = {
+        "msgtype": "text",
+        "text": {
+          "content": `转发邮件[${message.headers.get('subject')}]\n错误:${error}`
+        }
+      }
+      await sendToWebhook(body)
     }
 
-    listEvents.push(reportRow)
   }
-
-  return listEvents
-}
-
-async function sendToAnalyticsEngine(env: Env, reportRows: DmarcRecordRow[]) {
-  if (!env.DMARC_ANALYTICS) {
-    return
-  }
-
-  reportRows.forEach((recordRow, index) => {
-    const blobs: string[] = []
-    const doubles: number[] = []
-    const indexes: string[] = []
-
-    indexes.push(encodeURI(`${recordRow.reportMetadataReportId}-${index}`).slice(0, 32)) // max size 32 bytes
-
-    blobs.push(recordRow.reportMetadataReportId)
-    blobs.push(recordRow.reportMetadataOrgName)
-    doubles.push(recordRow.reportMetadataDateRangeBegin)
-    doubles.push(recordRow.reportMetadataDateRangeEnd)
-    blobs.push(recordRow.reportMetadataError)
-
-    blobs.push(recordRow.policyPublishedDomain)
-    doubles.push(recordRow.policyPublishedADKIM)
-    doubles.push(recordRow.policyPublishedASPF)
-    doubles.push(recordRow.policyPublishedP)
-    doubles.push(recordRow.policyPublishedSP)
-    doubles.push(recordRow.policyPublishedPct)
-
-    blobs.push(recordRow.recordRowSourceIP)
-    doubles.push(recordRow.recordRowCount)
-    doubles.push(recordRow.recordRowPolicyEvaluatedDKIM)
-    doubles.push(recordRow.recordRowPolicyEvaluatedSPF)
-    doubles.push(recordRow.recordRowPolicyEvaluatedDisposition)
-    doubles.push(recordRow.recordRowPolicyEvaluatedReasonType)
-    blobs.push(recordRow.recordIdentifiersEnvelopeTo)
-    blobs.push(recordRow.recordIdentifiersHeaderFrom)
-
-    env.DMARC_ANALYTICS.writeDataPoint({
-      blobs: blobs,
-      doubles: doubles,
-      indexes: indexes,
-    })
-  })
 }
